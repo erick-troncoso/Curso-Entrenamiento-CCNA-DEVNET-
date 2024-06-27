96eef6ef939e46e63b9a679fce91b1de5875e546
NetAcad DEVASC <changeme@example.com>
2024-06-27 20:04:25 +0000

    Devnet-002D

---
+++ solicitud_datos.py
@@ -0,0 +1,10 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+sede = input("Ingrese su sede: ")
+
+print("\nDatos ingresados:")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
+print(f"Sede: {sede}")
